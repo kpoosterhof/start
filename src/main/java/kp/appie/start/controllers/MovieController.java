1403c28ea00f99828607c8e48b397310793642d3
kp <16428428+kpoosterhof@users.noreply.github.com>
2018-06-19 15:31:38 +0200

    blah

--- src/main/java/kp/appie/start/controllers/MovieController.java
+++ src/main/java/kp/appie/start/controllers/MovieController.java
@@ -1,14 +1,20 @@
 package kp.appie.start.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import kp.appie.start.services.MovieService;
 
 @RestController
 public class MovieController {
 	
+	@Autowired
+	MovieService movieService;
+	
 	@RequestMapping("/")
 	public String showMovies() {
-		return "Movie";
+		return movieService.getMovie().toString();
 	}
 
 }
--- src/test/java/kp/appie/start/controller/MovieControllerTest.java
+++ src/test/java/kp/appie/start/controller/MovieControllerTest.java
@@ -2,7 +2,6 @@ package kp.appie.start.controller;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.fail;
 
 import org.junit.Before;
 import org.junit.Test;
